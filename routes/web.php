3b16f75f764acccf3c91c5acedecbee08b910575
Salvatore Scalzi <scalzi.salvatore@gmail.com>
2021-04-21 11:42:36 +0200

    upload config sqlsrv connection/ add web route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('sqlsrv', function () {
+    
+
+    $statement = DB::connection('sqlsrv')->statement('select * from [billoo].[Fatture_Testata] ');
+
+    dd($statement);
 });
